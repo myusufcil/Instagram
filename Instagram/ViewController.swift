8560aad3bd0efb7179aa46f3457dbac6b4064edc
Muhammed Yusuf ÇİL <ycil@partner.innova.com.tr>
2022-11-28 17:52:45 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by muhammed cil on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
